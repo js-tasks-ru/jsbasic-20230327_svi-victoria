3ed0cf13e162e813a315d6a918724a13877e12f5
Vika <svi.victoria@gmail.com>
2023-04-04 00:27:51 +0700

    Добавляю решение ДЗ Функция SumSalaries

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  summ = 0;
+  for (let key in salaries) {
+    let i = salaries[key];
+    if (Number.isFinite(i)) {
+      summ = summ + salaries[key];
+    }
+  }
+  return summ;
 }
