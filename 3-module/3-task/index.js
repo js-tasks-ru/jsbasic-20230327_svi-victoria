89c095ecc188eba99fc28a4e5e4fff6a7b8430ad
Vika <svi.victoria@gmail.com>
2023-04-08 08:18:13 +0700

    Добавляю решение ДЗ Преобразование строки

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  let newstr = str
+    .split("-")
+    .map((item, index, arr) =>
+      arr.indexOf(item) == 0 ? item : item[0].toUpperCase() + item.slice(1)
+    )
+    .join("");
+  return newstr;
 }
