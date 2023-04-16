100f1e8ec9a189c936484fb17c8fde2f0dc82089
Vika <svi.victoria@gmail.com>
2023-04-16 21:00:53 +0700

    Добавялю решение ДЗ Highlights

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,24 @@
 function highlight(table) {
-  // ваш код...
+  let tr = table.querySelectorAll("tbody tr");
+  for (let teacher of tr) {
+    let td = teacher.querySelectorAll("td");
+
+    if (td[2].textContent == "m") {
+      teacher.classList.add("male");
+    } else if (td[2].textContent == "f") {
+      teacher.classList.add("female");
+    }
+
+    if (td[1].textContent < 18) {
+      teacher.style = "text-decoration: line-through";
+    }
+
+    if (td[3].dataset.available == "true") {
+      teacher.classList.add("available");
+    } else if (td[3].dataset.available == "false") {
+      teacher.classList.add("unavailable");
+    } else if (td[3].dataset.available == null) {
+      teacher.hidden = true;
+    }
+  }
 }
