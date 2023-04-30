21ec74926824fe3bf02993f0a0be87e2a4b5003d
Vika <svi.victoria@gmail.com>
2023-04-30 10:57:48 +0700

    Добавляю ДЗ Лента меню для просмотра

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -1,7 +1,70 @@
-import createElement from '../../assets/lib/create-element.js';
+import { createElement } from "../../assets/lib/create-element.js";
 
 export default class RibbonMenu {
   constructor(categories) {
     this.categories = categories;
+    this.elem = createElement(`<div class="ribbon">
+    <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+    <button class="ribbon__arrow ribbon__arrow_right ribbon__arrow_visible">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+  </div>`);
+    this.inner = createElement(`<nav class="ribbon__inner"></nav>`);
+    this.buttonRigth = this.elem.querySelector(".ribbon__arrow_right");
+    this.buttonLeft = this.elem.querySelector(".ribbon__arrow_left");
+    this.elem.insertBefore(this.inner, this.buttonRigth);
+    for (let item of this.categories) {
+      this[item] = createCategories(item);
+      this.inner.append(this[item]);
+    }
+    this.menuItem = this.elem.querySelectorAll(".ribbon__item");
+    // let menuesActive = this.elem.querySelector(".ribbon__item_active");
+    // menuesActive.classList.remove("ribbon__item_active");
+    for (let menues of this.menuItem) {
+      menues.addEventListener("click", function (event) {
+        event.preventDefault();
+        menues.classList.add("ribbon__item_active");
+      });
+      menues.addEventListener("click", this.onClick);
+    }
+
+    this.elem.addEventListener("ribbon-select", (event) =>
+      console.log(event.detail)
+    );
+
+    this.initScrolling();
   }
+  onClick = (event) => {
+    let ribbonSelect = new CustomEvent("ribbon-select", {
+      detail: event.target.closest(".ribbon__item").dataset.id,
+      bubbles: true,
+    });
+    this.elem.dispatchEvent(ribbonSelect);
+  };
+  initScrolling() {
+    let innerforScrolling = this.inner;
+    let scrollLeft = innerforScrolling.scrollLeft;
+    let scrollWidth = innerforScrolling.scrollWidth;
+    let clientWidth = innerforScrolling.clientWidth;
+    let scrollRight = scrollWidth - scrollLeft - clientWidth;
+
+    this.buttonRigth.addEventListener("click", function (event) {
+      innerforScrolling.scrollBy(350, 0);
+      console.log(`scrollRight: ${scrollRight}`);
+    });
+
+    this.buttonLeft.addEventListener("click", function (event) {
+      innerforScrolling.scrollBy(-350, 0);
+      console.log(`scrollLeft: ${scrollLeft}`);
+    });
+  }
+}
+
+function createCategories(item) {
+  let oneCategory = createElement(
+    `<a href="#" class="ribbon__item " data-id="${item.id}">${item.name}</a>`
+  );
+  return oneCategory;
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -1,6 +1,54 @@
-import createElement from '../../assets/lib/create-element.js';
+import { createElement } from "../../assets/lib/create-element.js";
 
 export default class Modal {
   constructor() {
+    window.addEventListener("keydown", function (event) {
+      if (event.code === "Escape") {
+        this.close();
+      }
+    });
+  }
+  open() {
+    this.elem = createElement(`<div class="modal">
+    
+    <div class="modal__overlay"></div>
+
+    <div class="modal__inner">
+      <div class="modal__header">
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+        <h3 class="modal__title">
+          ${this.titletext}
+        </h3>
+      </div>
+      <div class="modal__body">
+        ${this.bodytext}
+      </div>
+    </div>
+
+  </div>`);
+    let body = document.querySelector("body");
+    body.append(this.elem);
+    body.classList.add("is-modal-open");
+    this.buttonClose = this.elem.querySelector(".modal__close");
+    this.buttonClose.addEventListener("click", this.close);
+  }
+  setTitle(titletext) {
+    this.titletext = titletext;
+  }
+  setBody(bodyinner) {
+    this.bodytext = bodyinner.textContent;
+  }
+  close() {
+    let body = document.querySelector("body");
+    body.classList.remove("is-modal-open");
+    let elem = document.querySelector(".modal");
+    elem.remove(); //не проходит тест, почему?
+    window.removeEventListener("keydown", function (event) {
+      if (event.code === "Escape") {
+        this.close();
+      }
+    });
   }
 }
