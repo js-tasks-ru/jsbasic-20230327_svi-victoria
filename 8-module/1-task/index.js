f2956395ca2c24978ab3e9b43521261c0523c926
Vika <svi.victoria@gmail.com>
2023-05-04 22:25:25 +0700

    Добавляю решение ДЗ Иконка корзины

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -1,10 +1,12 @@
-import createElement from '../../assets/lib/create-element.js';
+import createElement from "../../assets/lib/create-element.js";
 
 export default class CartIcon {
   constructor() {
     this.render();
 
     this.addEventListeners();
+    this.initialTopCoord =
+      this.elem.getBoundingClientRect().top + window.pageYOffset; //переместить в конструктор?
   }
 
   render() {
@@ -13,32 +15,59 @@ export default class CartIcon {
 
   update(cart) {
     if (!cart.isEmpty()) {
-      this.elem.classList.add('cart-icon_visible');
+      this.elem.classList.add("cart-icon_visible");
 
       this.elem.innerHTML = `
         <div class="cart-icon__inner">
           <span class="cart-icon__count">${cart.getTotalCount()}</span>
-          <span class="cart-icon__price">€${cart.getTotalPrice().toFixed(2)}</span>
+          <span class="cart-icon__price">€${cart
+            .getTotalPrice()
+            .toFixed(2)}</span>
         </div>`;
 
       this.updatePosition();
 
-      this.elem.classList.add('shake');
-      this.elem.addEventListener('transitionend', () => {
-        this.elem.classList.remove('shake');
-      }, {once: true});
-
+      this.elem.classList.add("shake");
+      this.elem.addEventListener(
+        "transitionend",
+        () => {
+          this.elem.classList.remove("shake");
+        },
+        { once: true }
+      );
     } else {
-      this.elem.classList.remove('cart-icon_visible');
+      this.elem.classList.remove("cart-icon_visible");
     }
   }
 
   addEventListeners() {
-    document.addEventListener('scroll', () => this.updatePosition());
-    window.addEventListener('resize', () => this.updatePosition());
+    document.addEventListener("scroll", () => this.updatePosition());
+    window.addEventListener("resize", () => this.updatePosition());
   }
 
   updatePosition() {
-    // ваш код ...
+    let firstIndent =
+      document.querySelector(".container").getBoundingClientRect().right + 20;
+    let secondIndent =
+      document.documentElement.clientWidth - this.elem.offsetWidth - 10;
+    let leftIndent = Math.min(firstIndent, secondIndent) + "px";
+
+    if (window.pageYOffset > this.initialTopCoord) {
+      Object.assign(this.elem.style, {
+        position: "fixed",
+        top: "50px",
+        zIndex: 1e3,
+        right: "10px",
+        left: leftIndent,
+      });
+    }
+    if (document.documentElement.clientWidth <= 767) {
+      Object.assign(this.elem.style, {
+        position: "",
+        top: "",
+        left: "",
+        zIndex: "",
+      });
+    }
   }
 }
