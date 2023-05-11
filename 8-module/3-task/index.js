d5efb1d04db501b6ca2d427a1164d3a42081bed0
Vika <svi.victoria@gmail.com>
2023-05-11 23:03:13 +0700

    Добавляю решение ДЗ 8.3. на проверку

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -1,28 +1,66 @@
 export default class Cart {
-  cartItems = []; // [product: {...}, count: N]
+  cartItems = [];
 
   constructor(cartIcon) {
     this.cartIcon = cartIcon;
   }
 
-  addProduct(product) {
-    // ваш код
+  addProduct(productToAdd) {
+    if (productToAdd === null) {
+      return;
+    } else {
+      let addObject = { product: productToAdd, count: 1 };
+      if (this.cartItems.length == 0) {
+        this.cartItems.push(addObject);
+      } else {
+        let filtratedItem = this.cartItems.filter(
+          (item) => item.product == productToAdd
+        );
+        if (filtratedItem.length == 0) {
+          this.cartItems.push(addObject);
+        } else {
+          let indexOfProduct = this.cartItems.findIndex(
+            (item) => item.product == filtratedItem[0].product
+          );
+          this.cartItems[indexOfProduct].count += 1;
+        }
+      }
+    }
+
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    let filtratedOnId = this.cartItems.filter(
+      (item) => item.product.id == productId
+    );
+    let indexOfProduct = this.cartItems.findIndex(
+      (item) => item.product == filtratedOnId[0].product
+    );
+    this.cartItems[indexOfProduct].count += amount;
+    if (this.cartItems[indexOfProduct].count == 0) {
+      delete this.cartItems[indexOfProduct];
+    }
+    this.onProductUpdate(cartItem);
   }
 
   isEmpty() {
-    // ваш код
+    let checkEmpty = this.cartItems.length == 0 ? true : false;
+    return checkEmpty;
   }
 
   getTotalCount() {
-    // ваш код
+    let totalCount = this.cartItems.reduce(function (total, item) {
+      return total + item.count;
+    }, 0);
+    return totalCount;
   }
 
   getTotalPrice() {
-    // ваш код
+    let totalPrice = this.cartItems.reduce(function (total, item) {
+      return total + item.product.price * item.count;
+    }, 0);
+    return totalPrice;
   }
 
   onProductUpdate(cartItem) {
@@ -31,4 +69,3 @@ export default class Cart {
     this.cartIcon.update(this);
   }
 }
-
